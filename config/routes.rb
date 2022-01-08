94da149a09e24379fc63dddc5845de559d97015e
rotimiazeez <harzeyzhrotimi@gmail.com>
2022-01-08 09:33:46 -0800

    feat: Add migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :activities
+  resources :categories
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/factories/activities.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :activity do
+    name { "MyString" }
+    amount { "9.99" }
+    user { nil }
+  end
+end
